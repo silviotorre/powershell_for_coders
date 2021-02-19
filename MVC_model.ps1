d8bd0c38ea87e57ca47239c0ba75207fa79b27a2
Silvio Torre <54410963+silviotorre@users.noreply.github.com>
2021-02-19 22:48:21 +0100

    Update MVC_model.ps1

--- MVC_model.ps1
+++ MVC_model.ps1
@@ -1,1 +1,18 @@
-Write-Host "Model MVC"
+class Computer {
+  [string] $Service
+  [string] $ClusterId
+  [string] $DatacenterId
+  [string] $Id
+  Computer([string]$Id) {
+    $this.Id = $Id
+    if ($Id -notmatch "(\w+)(\d+)(\w+\d+)") {
+      throw "Invalid Computer ID '$Id'"
+    }
+    $_, $this.Namespace, $this.ClusterId, $this.DatacenterId = $Matches
+  }
+}
+
+class AuditResult {
+  [boolean] $Healthy
+  [Computer] $Computer
+}
